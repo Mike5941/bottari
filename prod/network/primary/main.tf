4d111fd99ac53ad054485bc5058a51762ab18395
wonsoong <mike9159@naver.com>
2023-12-29 09:44:35 +0900

    alb

--- prod/network/primary/main.tf
+++ prod/network/primary/main.tf
@@ -12,11 +12,11 @@ terraform {
 module "primary" {
   source = "../../../modules/network"
 
-  project_name   = "primary"
-  vpc_cidr = "10.1.0.0/16"
+  project_name = "primary"
+  vpc_cidr     = "10.1.0.0/16"
 
 
-  server_arn = "arn:aws:acm:ap-northeast-2:675481538193:certificate/102a031f-0292-44be-825a-5001b0c517a7"
-  client_arn = "arn:aws:acm:ap-northeast-2:675481538193:certificate/9a11f97f-ebb1-4020-bc55-923a9e14bd4d"
+  server_arn = "arn:aws:acm:ap-northeast-2:617669297376:certificate/b713dd9e-c8c5-4672-a2f2-1fbbd24a1ecd"
+  client_arn = "arn:aws:acm:ap-northeast-2:617669297376:certificate/bc24aba4-1aa7-4bb8-9c78-a7786b5703a6"
 }
 
